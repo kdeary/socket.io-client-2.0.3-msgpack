7e0f0df5740f77b6888643f35260253f89d71447
raikutro <raikutro@gmail.com>
2022-04-06 09:00:20 -0700

    Compacter packets

---
+++ socket.io-msgpack-parser/index.js
@@ -0,0 +1,110 @@
+
+var msgpack = require('notepack.io');
+var Emitter = require('component-emitter');
+
+/**
+ * Packet types (see https://github.com/socketio/socket.io-protocol)
+ */
+
+exports.CONNECT = 0;
+exports.DISCONNECT = 1;
+exports.EVENT = 2;
+exports.ACK = 3;
+exports.ERROR = 4;
+exports.BINARY_EVENT = 5;
+exports.BINARY_ACK = 6;
+
+var isInteger = Number.isInteger || function (value) {
+  return typeof value === 'number' &&
+    isFinite(value) &&
+    Math.floor(value) === value;
+};
+
+var isString = function (value) { return typeof value === 'string'; };
+
+function Encoder () {}
+
+Encoder.prototype.encode = function (packet, callback) {
+  switch (packet.type) {
+    case exports.CONNECT:
+    case exports.DISCONNECT:
+    case exports.ERROR:
+      return callback([ JSON.stringify(packet) ]);
+    default:
+      return callback([ msgpack.encode(compactPacket(packet)) ]);
+  }
+};
+
+function Decoder () {}
+
+Emitter(Decoder.prototype);
+
+Decoder.prototype.add = function (obj) {
+  if (typeof obj === 'string') {
+    this.parseJSON(obj);
+  } else {
+    this.parseBinary(obj);
+  }
+};
+
+Decoder.prototype.parseJSON = function (obj) {
+  var decoded = JSON.parse(obj);
+  this.checkPacket(decoded);
+  this.emit('decoded', decoded);
+};
+
+Decoder.prototype.parseBinary = function (obj) {
+  var decoded = decompactPacket(msgpack.decode(obj));
+  this.checkPacket(decoded);
+  this.emit('decoded', decoded);
+};
+
+function compactPacket(packet) {
+  return [packet.type, packet.nsp, packet.data, packet.id];
+}
+
+function decompactPacket(packet) {
+  return {
+    type: packet[0],
+    nsp: packet[1],
+    data: packet[2],
+    id: packet[3]
+  };
+}
+
+function isDataValid (decoded) {
+  switch (decoded.type) {
+    case exports.CONNECT:
+    case exports.DISCONNECT:
+      return decoded.data === undefined;
+    case exports.ERROR:
+      return isString(decoded.data);
+    default:
+      return Array.isArray(decoded.data);
+  }
+}
+
+Decoder.prototype.checkPacket = function (decoded) {
+  var isTypeValid = isInteger(decoded.type) && decoded.type >= exports.CONNECT && decoded.type <= exports.BINARY_ACK;
+  if (!isTypeValid) {
+    throw new Error('invalid packet type');
+  }
+
+  if (!isString(decoded.nsp)) {
+    throw new Error('invalid namespace');
+  }
+
+  if (!isDataValid(decoded)) {
+    throw new Error('invalid payload');
+  }
+
+  var isAckValid = decoded.id === undefined || isInteger(decoded.id);
+  if (!isAckValid) {
+    throw new Error('invalid packet id');
+  }
+};
+
+Decoder.prototype.destroy = function () {};
+
+exports.Encoder = Encoder;
+exports.Decoder = Decoder;
